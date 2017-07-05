623fa24e150af9977a27c5c52186b54b253830e9
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2017-07-05 18:31:01 +0200

    no message

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    public AudioClip crack;
+    public Sprite[] hitSprites;
+    public static int breakableCount = 0;
+    public GameObject smoke;
+
+    private int timesHits;
+    private LevelManager levelManager;
+    private bool isBreakable; 
+
+    // Use this for initialization
+    void Start () {
+        isBreakable = (this.tag == "Breakable");
+        if (isBreakable)
+        {
+            breakableCount++;
+        }
+        timesHits = 0;
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        AudioSource.PlayClipAtPoint(crack, transform.position);
+        if (isBreakable)
+        {
+            HandleHits();
+        }
+        
+    }
+
+    void HandleHits()
+    {
+        timesHits++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHits >= maxHits)
+        {
+            breakableCount--;
+            levelManager.BrickDestroyed();
+            PuffSmoke();
+            Destroy(gameObject);
+            
+        }
+        else
+        {
+            LoadSprites();
+        }
+    }
+
+    void PuffSmoke()
+    {
+        GameObject smokePuf = Instantiate(smoke, transform.position, Quaternion.identity) as GameObject;
+        ParticleSystem.MainModule main = smokePuf.GetComponent<ParticleSystem>().main;
+        main.startColor = gameObject.GetComponent<SpriteRenderer>().color;
+
+        
+    }
+
+    void LoadSprites()
+    {
+        int spritesIndex = timesHits - 1;
+        if (hitSprites[spritesIndex])
+        {
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spritesIndex];
+        } else
+        {
+            Debug.LogError ("Sprite not Available for the brick");
+        }
+        
+    }
+
+    void SimulateWin()
+    {
+        levelManager.LoadNextLevel();
+    }
+}
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    private LevelManager levelManager;
+
+    void Start()
+    {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print("Trigger");
+        levelManager.LoadLevel("Loose Screen");
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+}
